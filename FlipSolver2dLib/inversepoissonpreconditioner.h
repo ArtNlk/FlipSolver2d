6a6613d6c26f10da3a69f88cb4cadc34a1248b6a
ArtNlk <artemthegood@gmail.com>
2023-11-07 01:32:27 +0100

    Incomplete poisson preconditioner part 2

--- FlipSolver2dLib/inversepoissonpreconditioner.h
+++ FlipSolver2dLib/inversepoissonpreconditioner.h
@@ -1,6 +1,7 @@
 #ifndef INVERSEPOISSONPRECONDITIONER_H
 #define INVERSEPOISSONPRECONDITIONER_H
 
+#include <Eigen/Core>
 #include <Eigen/Sparse>
 
 template <typename Scalar, int UpLo_ = Eigen::Lower, typename OrderingType_ = Eigen::AMDOrdering<int> >
@@ -14,11 +15,7 @@ public:
     typedef OrderingType_ OrderingType;
     typedef typename OrderingType::PermutationType PermutationType;
     typedef typename PermutationType::StorageIndex StorageIndex;
-    typedef Eigen::SparseMatrix<Scalar,Eigen::ColMajor,StorageIndex> FactorType;
-    typedef Eigen::Matrix<Scalar,Eigen::Dynamic,1> VectorSx;
-    typedef Eigen::Matrix<RealScalar,Eigen::Dynamic,1> VectorRx;
-    typedef Eigen::Matrix<StorageIndex,Eigen::Dynamic, 1> VectorIx;
-    typedef std::vector<std::list<StorageIndex> > VectorList;
+    typedef Eigen::SparseMatrix<Scalar,Eigen::RowMajor,StorageIndex> FactorType;
     enum { UpLo = UpLo_ };
     enum {
         ColsAtCompileTime = Eigen::Dynamic,
@@ -45,8 +42,37 @@ public:
         return m_info;
     }
 
-    void factorize(const Eigen::SparseMatrix<double,Eigen::Upper>& mat)
+    void factorize(const Eigen::SparseMatrix<double,Eigen::RowMajor>& mat)
     {
+        auto identity = mat;
+        identity.setIdentity();
+        Eigen::VectorXd invdiag;
+        invdiag.resize(mat.cols());
+
+#pragma omp parallel for
+        for(int j=0; j<mat.outerSize(); ++j)
+        {
+            Eigen::SparseMatrix<double,Eigen::RowMajor>::InnerIterator it(mat,j);
+            while(it && it.index()!=j) ++it;
+            if(it && it.index()==j && it.value()!=Scalar(0))
+                invdiag(j) = Scalar(1)/it.value();
+            else
+                invdiag(j) = Scalar(1);
+        }
+        m_mat = mat.template triangularView<Eigen::StrictlyLower>();
+
+#pragma omp parallel for
+        for(int j=0; j<m_mat.outerSize(); ++j)
+        {
+            Eigen::SparseMatrix<double,Eigen::RowMajor>::InnerIterator it(m_mat,j);
+            for(;it && it.index() < j; ++it)
+            {
+                it.valueRef() *= invdiag(it.col());
+            }
+        }
+        m_mat = identity - m_mat;
+        //m_mat = m_mat * m_mat.transpose();
+
         m_info = Eigen::Success;
         return;
     }
@@ -62,7 +88,8 @@ public:
     void _solve_impl(const Rhs& b, Dest& x) const
     {
         eigen_assert(m_factorizationIsOk && "factorize() should be called first");
-        x = b;
+        x = m_mat * (m_mat.transpose() * b);
+        //x = m_mat.adjoint().template triangularView<Eigen::Lower>() * x;
     }
 
 protected:
