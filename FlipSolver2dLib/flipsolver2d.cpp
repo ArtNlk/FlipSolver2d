f8fac9b00cf3f2782c12edc1eba3c8617d9f4c45
ArtNlk <artemthegood@gmail.com>
2025-02-08 18:18:19 +0100

    Fixed viscosty to grid transfer

--- FlipSolver2dLib/flipsolver2d.cpp
+++ FlipSolver2dLib/flipsolver2d.cpp
@@ -1399,7 +1399,7 @@ void FlipSolver::centeredParamsToGridThread(Range r)
         Index2d i2d = m_fluidVelocityGrid.index2d(idx);
         std::array<ssize_t,9> affectingBins = m_markerParticles.binsForGridCell(i2d);
 
-        float weightTotal = 0.0;
+        float weightTotal = 1e-10;
         float viscVal = 0.0;
         bool viscValid = false;
 
